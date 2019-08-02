ecef0bc2e14f76f2e962012a3672a5c9c80e79b0
Joanna Hauza <joannahauza@gmail.com>
2019-08-02 16:25:44 +0200

    zapytania do bazy danych - metody w klasie User

--- src/pl/joanna026/progschool/app/TestApp.java
+++ src/pl/joanna026/progschool/app/TestApp.java
@@ -1,12 +1,87 @@
 package pl.joanna026.progschool.app;
 
+
+
+import pl.joanna026.progschool.dao.UserDAO;
+import pl.joanna026.progschool.model.User;
 import pl.joanna026.progschool.util.DBUtil;
 
+
 import java.sql.SQLException;
+import java.util.Arrays;
 
 public class TestApp {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
+
+//        testConnect();
+//        testCreateUser();
+//        testReadUser();
+//        testUpdateUser();
+//        testDeleteUser();
+
+        testFindAll();
+
+
+    }
+
+    private static void testFindAll() {
+        UserDAO userDAO= new UserDAO();
+        User user1= new User("test1", "test1", "test1");
+        User user2= new User("test2", "test2", "test2");
+        User user3= new User("test3", "test3", "test3");
+
+
+        userDAO.create(user1);
+        userDAO.create(user2);
+        userDAO.create(user3);
+
+        User[] allUsers =userDAO.findAll();
+        System.out.println(Arrays.toString(allUsers));
+    }
+
+    private static void testDeleteUser() {
+        UserDAO userDAO= new UserDAO();
+        User user=userDAO.read(3);
+
+        userDAO.delete(user.getId());
+
+        User userAfterDelete=userDAO.read(user.getId());
+        System.out.println(userAfterDelete);
+    }
+
+    private static void testUpdateUser() {
+        UserDAO userDAO= new UserDAO();
+        User user=userDAO.read(3);
+        user.setUsername("newusername");
+
+        userDAO.update(user);
+
+        User userAfterUpdate=userDAO.read(3);
+        System.out.println(userAfterUpdate);
+    }
+
+    private static void testReadUser() {
+        UserDAO userDAO=new UserDAO();
+        User user=userDAO.read(3);
+        System.out.println(user);
+
+        User user2 =userDAO.read(101);
+        System.out.println(user2);
+    }
+
+    private static void testConnect() throws SQLException {
         DBUtil.connect();
     }
+
+    private static void testCreateUser(){
+        User user = new User("test1@test.pl", "test", "test");
+        System.out.println(user);
+
+        UserDAO userDAO = new UserDAO();
+        user = userDAO.create(user);
+
+        System.out.println(user);
+    }
+
 }
--- src/pl/joanna026/progschool/dao/UserDAO.java
+++ src/pl/joanna026/progschool/dao/UserDAO.java
@@ -1,7 +1,117 @@
 package pl.joanna026.progschool.dao;
 
+import pl.joanna026.progschool.model.User;
+import pl.joanna026.progschool.util.DBUtil;
+
+import java.sql.*;
+import java.util.Arrays;
+
 public class UserDAO {
 
+    private static final String CREATE_USER_QUERY = "INSERT INTO users (email, username, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?";
+    private static final String UPDATE_USER_QUERY = "UPDATE users SET email=?, username=?, password=? WHERE id=?";
+    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id=?";
+    private static final String FIND_ALL_USERS_QUERY = "SELECT * FROM users";
 
 
+    public User create(User user) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+
+            //Ustawienie parametrów zapytania (wartości do wstawienia)
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUsername());
+            statement.setString(3, user.getPassword());
+
+            //Wykonanie zapytania
+            statement.executeUpdate();
+
+            // Pobranie zestawu wygenerowanych kluczy
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.first()) {
+                int generatedKey = generatedKeys.getInt(1);
+                user.setId(generatedKey);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public User read(int id) {
+
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(READ_USER_QUERY);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void update(User user) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_USER_QUERY);
+            statement.setString(1, user.getEmail());
+            statement.setString(2, user.getUsername());
+            statement.setString(3, user.getPassword());
+            statement.setInt(4, user.getId());
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void delete(int id) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_USER_QUERY);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAll() {
+        try (Connection conn = DBUtil.connect()) {
+            User[] users = new User[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_USERS_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+                users = addToArray(user, users);
+            }
+            return users;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1);
+        tmpUsers[users.length] = u;
+        return tmpUsers;
+    }
 }
